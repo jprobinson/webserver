6be379dac46beb3b04b339663c85a475dca6fff7
JP Robinson <john.paul.robinson@gmail.com>
2013-11-03 17:50:12 -0500

    added gorilla mux and some access logging

--- personal/server.go
+++ personal/server.go
@@ -3,8 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/jprobinson/go-utils/utils"
+	"github.com/jprobinson/go-utils/web"
+)
+
+const (
+	serverLog = "/var/log/goserver/server.log"
+	accessLog = "/var/log/goserver/access.log"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe("jprbnsn.com", http.FileServer(http.Dir("/home/jp/www"))))
+	logSetup := &utils.DefaultLogSetup{LogFile: serverLog}
+	logSetup.SetupLogging()
+	go utils.ListenForLogSignal(logSetup)
+
+	router := mux.NewRouter()
+
+	jpRouter := router.Host("jprbnsn.com").Subrouter()
+	jpRouter.PathPrefix("/").Handler(http.FileServer(http.Dir("/home/jp/www")))
+
+	wwwJPRouter := router.Host("www.jprbnsn.com").Subrouter()
+	wwwJPRouter.PathPrefix("/").Handler(http.FileServer(http.Dir("/home/jp/www")))
+
+	apiHandler := web.AccessLogHandler(accessLog, router)
+
+	http.Handle("/", apiHandler)
+
+	log.Fatal(http.ListenAndServe(":80", apiHandler))
 }
