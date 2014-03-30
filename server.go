0c7d323447c5af3e362c9bd029bfd444ba83a476
JP Robinson <john.paul.robinson@gmail.com>
2014-03-30 16:48:08 +0000

    added newshound subdomain

--- server.go
+++ server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -8,22 +11,32 @@ import (
 
 	"github.com/jprobinson/go-utils/utils"
 	"github.com/jprobinson/go-utils/web"
+	"github.com/jprobinson/newshound/web/webserver/api"
 )
 
 const (
 	serverLog = "/var/log/goserver/server.log"
 	accessLog = "/var/log/goserver/access.log"
+
+	configFile   = "/opt/newshound/etc/config.json"
+	newshoundWeb = "/opt/newshound/www"
 )
 
 func main() {
-	logSetup := &utils.DefaultLogSetup{LogFile: serverLog}
+	logSetup := utils.NewDefaultLogSetup(serverLog)
 	logSetup.SetupLogging()
 	go utils.ListenForLogSignal(logSetup)
 
 	router := mux.NewRouter()
 
-        kat := router.Host("23.239.9.103").Subrouter()
-        kat.PathPrefix("/").Handler(http.FileServer(http.Dir("/home/jp/www/kathrynforestdesign")))
+	// newshound setup
+	config := NewConfig()
+	newshoundAPI := api.NewNewshoundAPI(config.DBURL, config.DBUser, config.DBPassword)
+	// add newshound server
+	newshound := router.Host("newshound.jprbnsn.com").Subrouter()
+	subApiRouter := newshound.PathPrefix(newshoundAPI.UrlPrefix()).Subrouter()
+	newshoundAPI.Handle(subApiRouter)
+	newshoundSub.PathPrefix("/").Handler(http.FileServer(http.Dir(newshoundWeb)))
 
 	jpRouter := router.Host("jprbnsn.com").Subrouter()
 	jpRouter.PathPrefix("/").Handler(http.FileServer(http.Dir("/home/jp/www/jprbnsn")))
@@ -34,4 +47,26 @@ func main() {
 	handler := web.AccessLogHandler(accessLog, router)
 
 	log.Fatal(http.ListenAndServe(":80", handler))
+}
+
+type config struct {
+	DBURL      string `json:"db-url"`
+	DBUser     string `json:"db-user"`
+	DBPassword string `json:"db-pw"`
+}
+
+func NewConfig() *config {
+	config := config{}
+
+	readBytes, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		panic(fmt.Sprintf("Cannot read config file: %s %s", config, err))
+	}
+
+	err = json.Unmarshal(readBytes, &config)
+	if err != nil {
+		panic(fmt.Sprintf("Cannot parse JSON in config file: %s %s", config, err))
+	}
+
+	return &config
 }
