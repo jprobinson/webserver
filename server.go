f92d0ca9346545598b0ab914e3f3560368ff79bb
JP Robinson <john.paul.robinson@gmail.com>
2017-07-16 12:15:27 -0400

    adding autocert & redirecting wheresthel

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"io/ioutil"
 	"log"
@@ -9,23 +10,20 @@ import (
 	"net/url"
 
 	"github.com/gorilla/mux"
-
 	"github.com/jprobinson/go-utils/utils"
 	"github.com/jprobinson/go-utils/web"
+	"golang.org/x/crypto/acme/autocert"
+
 	"github.com/jprobinson/newshound/web/webserver/api"
-	"github.com/jprobinson/webserver/api/subway"
 )
 
 const (
 	serverLog = "/var/log/goserver/server.log"
 	accessLog = "/var/log/goserver/access.log"
 
-	houndConfig  = "/opt/newshound/etc/config.json"
-	subwayConfig = "/opt/jp/etc/keys.json"
+	houndConfig = "/opt/newshound/etc/config.json"
 
 	newshoundWeb = "/opt/newshound/www"
-	wheresLWeb   = "/opt/jp/www/ltrain"
-	subwayWeb    = "/opt/jp/www/subway"
 )
 
 func main() {
@@ -35,24 +33,14 @@ func main() {
 
 	router := mux.NewRouter()
 
-	// add subway stuffs to server
-	sconfig := NewConfig(subwayConfig)
-	setupSubway(router, sconfig, subwayWeb, "subway.jprbnsn.com")
-	setupSubway(router, sconfig, subwayWeb, "wheresthetrain.nyc")
-	setupSubway(router, sconfig, subwayWeb, "www.wheresthetrain.nyc")
-	setupSubway(router, sconfig, subwayWeb, "wtt.nyc")
-	setupSubway(router, sconfig, subwayWeb, "www.wtt.nyc")
-	setupSubway(router, sconfig, wheresLWeb, "wheresthel.com")
-	setupSubway(router, sconfig, wheresLWeb, "www.wheresthel.com")
+	// add subway redirects to server
+	setupSubway(router, "subway.jprbnsn.com")
+	setupSubway(router, "wheresthel.com")
+	setupSubway(router, "www.wheresthel.com")
 
-	// add subway stuffs to server
 	// add the countdown
 	countdownRouter := router.Host("countdown.jprbnsn.com").Subrouter()
 	countdownRouter.PathPrefix("/").Handler(http.FileServer(http.Dir("/opt/jp/www/thecountdown")))
-
-	// add wg4gl
-	setupWG4GL(router, "wg4gl.com")
-	setupWG4GL(router, "www.wg4gl.com")
 
 	setupColin(router, "colinjhiggins.com")
 	setupColin(router, "www.colinjhiggins.com")
@@ -78,23 +66,35 @@ func main() {
 
 	handler := web.AccessLogHandler(accessLog, router)
 
-	log.Fatal(http.ListenAndServe(":80", handler))
+	certManager := autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(
+			"jprbnsn.com", "www.jprbnsn.com",
+			"newshound.jprbnsn.com",
+			"colinjhiggins.com", "www.colinjhiggins.com",
+		),
+		Cache: autocert.DirCache("certs"),
+	}
+
+	// http
+	go log.Fatal(http.ListenAndServe(":80", handler))
+
+	// https
+	server := &http.Server{
+		Addr: ":443",
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+	log.Fatal(server.ListenAndServeTLS("", ""))
 }
 
-func setupWG4GL(router *mux.Router, host string) {
-	wgRouter := router.Host(host).Subrouter()
-	wgRouter.PathPrefix("/").Handler(http.FileServer(http.Dir("/opt/jp/www/wg4gl")))
-}
-
-func setupSubway(router *mux.Router, sconfig *config, www, host string) {
-	subwayAPI := subway.NewSubwayAPI(sconfig.SubwayKey)
-	// add subway subdomain to webserver
-	subwayRouter := router.Host(host).Subrouter()
-	// add subways's API to the subdomain
-	subwayAPIRouter := subwayRouter.PathPrefix(subwayAPI.UrlPrefix()).Subrouter()
-	subwayAPI.Handle(subwayAPIRouter)
-	// add subway UI to to the subdomain...web we have one
-	subwayRouter.PathPrefix("/").Handler(http.FileServer(http.Dir(www)))
+func setupSubway(router *mux.Router, host string) {
+	router.Host(host).Handler(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "http://wheresthetrain.nyc", http.StatusMovedPermanently)
+		}),
+	)
 }
 
 func setupJP(router *mux.Router, host string) {
@@ -111,8 +111,6 @@ type config struct {
 	DBURL      string `json:"db-url"`
 	DBUser     string `json:"db-user"`
 	DBPassword string `json:"db-pw"`
-
-	SubwayKey string
 
 	NewRelicKey string
 }
